d16582d7a0fa03aea4e9a42d9259b89590b4fea8
Deem3 <erdemee.t@gmail.com>
2023-04-21 23:50:22 +0800

    new file:   aboutUs.html 	new file:   address.html 	new file:   cart.html 	new file:   director.html 	new file:   eachProduct.html 	new file:   images/ Cart.svg 	new file:   images/ Users.svg 	new file:   images/1.png 	new file:   images/2.png 	new file:   images/3.png 	new file:   images/Logo.png 	new file:   images/Navbar.svg 	new file:   images/approval_1.png 	new file:   images/condition/bottle.png 	new file:   images/condition/calories.png 	new file:   images/condition/celsius.png 	new file:   images/condition/keep.png 	new file:   images/condition/products.png 	new file:   images/footer/facebook.svg 	new file:   images/footer/instagram.svg 	new file:   images/footer/mail.svg 	new file:   images/forbidden__2__1.png 	new file:   images/image_1.png 	new file:   images/image_2.png 	new file:   images/product/rectangle_32.png 	new file:   images/product/rectangle_38.png 	new file:   images/product/rectangle_39.png 	new file:   images/product/shopping_cart.svg 	new file:   images/rectangle51.png 	new file:   images/rectangle_38-transformed.png 	new file:   images/rectangle_38.png 	new file:   images/rectangle_39.png 	new file:   images/rectangle_40 (1).png 	new file:   images/rectangle_40 (2).png 	new file:   images/rectangle_40 (3).png 	new file:   images/rectangle_40 (4).png 	new file:   images/rectangle_40 (5).png 	new file:   images/rectangle_40.png 	new file:   images/rectangle_44.png 	new file:   images/rectangle_51.png 	new file:   images/rectangle_52.png 	new file:   images/social/ Message.svg 	new file:   images/social/email.svg 	new file:   images/social/facebook.svg 	new file:   images/social/insta.svg 	new file:   images/social/mail.svg 	new file:   images/technik.png 	new file:   images/techno.png 	new file:   index.html 	new file:   js/Footer.js 	new file:   js/Navbar.js 	new file:   js/cart/cartInfo.js 	new file:   js/cart/cartList.js 	new file:   js/eachProduct/productDetails.js 	new file:   js/index/index.js 	new file:   js/product/addCart.js 	new file:   js/product/productList.js 	new file:   json/products.json 	new file:   product.html 	new file:   styles/cart/cart.css 	new file:   styles/eachProduct/eachProduct.css 	new file:   styles/global style/footer.css 	new file:   styles/global style/navbar.css 	new file:   styles/index/index.css 	new file:   styles/product/product.css 	new file:   styles/root.css 	new file:   technology.html

---
+++ js/product/addCart.js
@@ -0,0 +1,21 @@
+'use strict'
+
+var count = 0;
+
+const handleAdd = (id) => {
+    count++
+
+    localStorage.setItem(id, count);
+}
+
+const handleProd = (id) => {
+    const _id = localStorage.getItem('id')
+    if(_id !== null){
+        localStorage.removeItem('id')
+        localStorage.setItem('id', id)
+    }
+    else{
+        localStorage.setItem('id', id)
+    }
+        window.location.href = 'eachProduct.html'
+}
---
+++ js/product/productList.js
@@ -0,0 +1,111 @@
+class ProductList extends HTMLElement {
+    constructor() {
+        super();
+        // create shadowDOM
+        this.attachShadow({ mode: 'open' });
+        // this is style template
+        this.articleStyle = `
+        section{
+            display: grid;
+            grid-template-columns: repeat(3, 1fr);
+            justify-items: center;
+            justify-content: center;
+            align-items: center;
+            height: 80rem;
+        }
+        article{
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            position: relative;
+        }
+        
+        .product-img{
+            width: 12rem;
+            height: 12rem;;
+        }
+        
+        .product-cart{
+            position: absolute;
+            bottom: 25%;
+            right: 0;
+            width: 3.125rem;
+            height: 3.125rem;
+        }
+        
+        .product-info{
+            display: grid;   
+            grid-template-columns: repeat(3, 1fr);
+            grid-template-rows: 1fr;
+            align-items: center;
+            justify-items: center;
+            border-top: 1px solid gray;
+            margin-top: 1.25rem;
+            width: 25rem;
+        }
+        
+        .product-info>p{
+            font-size: 1rem;
+            color: gray;
+        }
+        .product-info>h3{
+            font-weight: 100;
+            font-size: 1.5rem;
+        }`
+        // creating section element and named it as section and adding it to shadowDOM
+        this.section = document.createElement('section')
+        this.shadowRoot.appendChild(this.section)
+    }
+
+    // this is render function which parameter is product
+
+    #Render(product){
+        const article = document.createElement('article');
+        article.innerHTML = `
+        <img src="${product.img}" alt="${product.name}" class="product-img">
+            <img src="images/product/shopping_cart.svg" class="product-cart" onclick="handleAdd(${product._id})">
+            <div class="product-info">
+            <p>${product.weight} гр</p>
+            <h3 onclick="handleProd(${product._id})">${product.name}</h3>
+            </div>
+        `
+        // add this article to section
+        this.shadowRoot.querySelector('section').appendChild(article);
+    }
+
+    // this function will fire this code when this component will be added to DOM
+
+    connectedCallback() {
+        // creating style element and applying style template to it after that adding it to shadowDOM
+        const style = document.createElement('style');
+        style.innerHTML = this.articleStyle;
+        this.shadowRoot.appendChild(style);
+        
+        // fetch data from api
+        fetch("https://api.jsonbin.io/v3/b/643eae58c0e7653a05a6e439")
+            .then(res => res.json())
+            .then(data => {
+                const products =data.record.products;
+                // loop through products and render them
+                products.map(product => {
+                    this.#Render(product);
+                })
+            })
+            .catch(err => console.log(err));
+        }
+
+    disconnectedCallback() {
+        //implementation
+    }
+
+    attributeChangedCallback(name, oldVal, newVal) {
+        //implementation
+    }
+
+    adoptedCallback() {
+        //implementation
+    }
+
+}
+
+window.customElements.define('product-list', ProductList);
