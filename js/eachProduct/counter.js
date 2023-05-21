f6be3a116b46315b8b00452b8b3dc4f609d1c44c
Deem3 <erdemee.t@gmail.com>
2023-05-21 14:06:53 +0800

    modified:   eachProduct.html 	modified:   js/eachProduct/counter.js 	modified:   js/eachProduct/eachProduct.js

--- js/eachProduct/counter.js
+++ js/eachProduct/counter.js
@@ -1,91 +1,91 @@
-'use strict'
+// 'use strict'
 
 
-// observing dom changes
-const targetNode = document.querySelector('product-details')
-console.log(targetNode)
+// // observing dom changes
+// const targetNode = document.querySelector('product-details')
+// console.log(targetNode)
 
-var loaded = false;
+// var loaded = false;
 
-const config = { attributes: true, childList: true, subtree: true };
+// const config = { attributes: true, childList: true, subtree: true };
 
-const observeDomChanges = (callback) => {
-  const observer = new MutationObserver((mutationsList) => {
-    for (let mutation of mutationsList) {
-      if (mutation.type === 'childList' || mutation.type === 'attributes') {
-        callback();
-        return;
-      }
-    }
-  });
+// const observeDomChanges = (callback) => {
+//   const observer = new MutationObserver((mutationsList) => {
+//     for (let mutation of mutationsList) {
+//       if (mutation.type === 'childList' || mutation.type === 'attributes') {
+//         callback();
+//         return;
+//       }
+//     }
+//   });
   
-  observer.observe(targetNode, config);
+//   observer.observe(targetNode, config);
 
-  setInterval(()=>{
-    if(loaded == true)
-    clearInterval()
-    observer.disconnect()
-  }, 100)
-};
-
-
-observeDomChanges(() => {
-  loaded = true;
-  console.log('DOM changed!');
-});
+//   setInterval(()=>{
+//     if(loaded == true)
+//     clearInterval()
+//     observer.disconnect()
+//   }, 100)
+// };
 
 
+// observeDomChanges(() => {
+//   loaded = true;
+//   console.log('DOM changed!');
+// });
 
-// if dom changed then execute this code
-const checkLoaded = setInterval(()=>{
-  if(loaded){
-    console.log('stop')
-    clearInterval(checkLoaded)
-    const num = document.getElementById("num");
-    console.log(num)
+
+
+// // if dom changed then execute this code
+// const checkLoaded = setInterval(()=>{
+//   if(loaded){
+//     console.log('stop')
+//     clearInterval(checkLoaded)
+//     const num = document.getElementById("num");
+//     console.log(num)
   
-    const globalState = (() => {
-      let counter = 1;
+//     const globalState = (() => {
+//       let counter = 1;
       
-      const incrementCounter = () => {
-        counter++;
-        num.textContent = counter;
-      };
+//       const incrementCounter = () => {
+//         counter++;
+//         num.textContent = counter;
+//       };
       
-      const decrementCounter = () => {
-        if (counter > 1) {
-          counter--;
-          num.textContent = counter;
-        }
-      };
+//       const decrementCounter = () => {
+//         if (counter > 1) {
+//           counter--;
+//           num.textContent = counter;
+//         }
+//       };
       
-      const order = () => {
-        const myProd = {
-          "id": localStorage.getItem('id'),
-          "stock": counter
-        };
-        const jsonOrder = JSON.stringify(myProd);
-        localStorage.setItem(localStorage.getItem('id'), jsonOrder);
-      };
+//       const order = () => {
+//         const myProd = {
+//           "id": localStorage.getItem('id'),
+//           "stock": counter
+//         };
+//         const jsonOrder = JSON.stringify(myProd);
+//         localStorage.setItem(localStorage.getItem('id'), jsonOrder);
+//       };
       
-      return {
-        getCounter: () => counter,
-        incrementCounter,
-        decrementCounter,
-        order
-      };
-    })();
+//       return {
+//         getCounter: () => counter,
+//         incrementCounter,
+//         decrementCounter,
+//         order
+//       };
+//     })();
   
-    document.getElementById('plus_btn').addEventListener('click', () => {
-      globalState.incrementCounter();
-    });
+//     document.getElementById('plus_btn').addEventListener('click', () => {
+//       globalState.incrementCounter();
+//     });
   
-    document.getElementById('minus_btn').addEventListener('click', () => {
-      globalState.decrementCounter();
-    });
+//     document.getElementById('minus_btn').addEventListener('click', () => {
+//       globalState.decrementCounter();
+//     });
   
-    document.getElementById('order').addEventListener('click', () => {
-      globalState.order();
-    });
-  }
-}, 100)
+//     document.getElementById('order').addEventListener('click', () => {
+//       globalState.order();
+//     });
+//   }
+// }, 100)
