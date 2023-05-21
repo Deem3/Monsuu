cf75a10e1fb18749cbaa0ee6c1308c6c42f77862
Deem3 <erdemee.t@gmail.com>
2023-05-21 14:04:47 +0800

    new file:   README.md 	deleted:    address.html 	modified:   cart.html 	deleted:    createProduct.html 	modified:   eachProduct.html 	renamed:    images/header/cart.svg -> images/ Cart.svg 	renamed:    images/header/users.svg -> images/ Users.svg 	new file:   images/1.png 	deleted:    images/1.webp 	new file:   images/2.png 	deleted:    images/2.webp 	new file:   images/3.png 	deleted:    images/3.webp 	new file:   images/approval_1.png 	deleted:    images/approval_1.webp 	new file:   images/condition/bottle.png 	deleted:    images/condition/bottle.webp 	new file:   images/condition/calories.png 	deleted:    images/condition/calories.webp 	new file:   images/condition/celsius.png 	deleted:    images/condition/celsius.webp 	new file:   images/condition/keep.png 	deleted:    images/condition/keep.webp 	new file:   images/condition/products.png 	deleted:    images/condition/products.webp 	deleted:    images/favico/android-chrome-192x192.png 	deleted:    images/favico/android-chrome-512x512.png 	deleted:    images/favico/apple-touch-icon.png 	deleted:    images/favico/favicon-16x16.png 	deleted:    images/favico/favicon-32x32.png 	deleted:    images/favico/favicon.ico 	deleted:    images/favico/site.webmanifest 	modified:   images/footer/facebook.svg 	new file:   images/forbidden__2__1.png 	deleted:    images/forbidden__2__1.webp 	deleted:    images/header/Logo.webp 	deleted:    images/header/icons8-shopping-cart-48.webp 	new file:   images/image_1.png 	deleted:    images/image_1.webp 	new file:   images/image_2.png 	deleted:    images/image_2.webp 	deleted:    images/payment/capitron.png 	deleted:    images/payment/golomt.png 	deleted:    images/payment/khanbank.png 	deleted:    images/payment/mbank.png 	deleted:    images/payment/qpay.png 	deleted:    images/payment/simple.jpeg 	deleted:    images/payment/tdb.png 	new file:   images/product/rectangle_32.png 	deleted:    images/product/rectangle_32.webp 	new file:   images/product/rectangle_38.png 	deleted:    images/product/rectangle_38.webp 	new file:   images/product/rectangle_39.png 	deleted:    images/product/rectangle_39.webp 	deleted:    images/rectangle51.webp 	new file:   images/rectangle_38-transformed.png 	deleted:    images/rectangle_38-transformed.webp 	new file:   images/rectangle_38.png 	deleted:    images/rectangle_38.webp 	new file:   images/rectangle_39.png 	deleted:    images/rectangle_39.webp 	deleted:    images/rectangle_40 (1).webp 	deleted:    images/rectangle_40 (2).webp 	deleted:    images/rectangle_40 (3).webp 	deleted:    images/rectangle_40 (4).webp 	deleted:    images/rectangle_40 (5).webp 	deleted:    images/rectangle_40.webp 	new file:   images/rectangle_44.png 	deleted:    images/rectangle_44.webp 	deleted:    images/rectangle_51.webp 	deleted:    images/rectangle_52.webp 	deleted:    images/technik.webp 	deleted:    images/techno.webp 	modified:   index.html 	modified:   js/Footer.js 	modified:   js/Navbar.js 	deleted:    js/cart/cartInfo.js 	deleted:    js/cart/cartList.js 	deleted:    js/cart/paymentPopup.js 	deleted:    js/cart/popupPay.js 	deleted:    js/create-product.js 	new file:   js/eachProduct/counter.js 	new file:   js/eachProduct/eachProduct.js 	deleted:    js/eachProduct/productDetails.js 	modified:   js/product/addCart.js 	deleted:    js/product/productList.js 	new file:   js/product/products.js 	new file:   json/products.json 	modified:   product.html 	deleted:    styles/address/address.css 	modified:   styles/cart/cart.css 	modified:   styles/eachProduct/eachProduct.css 	modified:   styles/global style/footer.css 	deleted:    styles/global style/footer.mobile..css 	modified:   styles/global style/navbar.css 	deleted:    styles/global style/navbar.mobile.css 	modified:   styles/index/index.css 	deleted:    styles/index/index.mobile.css 	deleted:    styles/index/index.tablet.css 	modified:   styles/product/product.css 	modified:   styles/root.css

---
+++ js/eachProduct/eachProduct.js
@@ -0,0 +1,81 @@
+'use strict'
+
+
+class ProductDetails extends HTMLElement {
+    constructor() {
+      super();
+  
+      // Get the product id from localStorage
+      const id = localStorage.getItem('id');
+  
+      // Fetch the product data
+      fetch('/json/products.json')
+        .then((response) => response.json())
+        .then(data => {
+          const products = data.products[id - 1];
+          this.render(products);
+        })
+        .catch(error => console.log(error));
+
+    }
+  
+    render(products) {
+      this.innerHTML = `
+        <section class="main">
+          <section class="product">
+            <img src="${products.img}" alt="${products.name}" class="product-img">
+            <h2>${products.name}</h2>
+            <h4>${products.price}₮</h4>
+            <div class="counter_btn">
+              <button class="minus_btn" id="minus_btn">-</button>
+              <p class="num" id="num">1</p>
+              <button class="plus_btn" id="plus_btn">+</button>
+            </div>
+            <button class="order_btn" id="order">Захиалах</button>
+          </section>
+          <section class="product-info">
+            <article>
+              <img src="/images/condition/products.png"></img>
+              <h3>САВЛАГАА</h3>
+              <p>${products.package} </p>
+            </article>
+            <article>
+              <img src="/images/condition/bottle.png"></img>
+              <h3>ХЭМЖЭЭ</h3>
+              <p>${products.weight}</p>
+            </article>
+            <article>
+              <img src="/images/condition/calories.png"></img>
+              <h3>ИЛЧЛЭГ</h3>
+              <p>${products.calore} Ккал</p>
+            </article>
+            <article>
+              <img src="/images/condition/keep.png"></img>
+              <h3>ХАДГАЛАХ ХУГАЦАА</h3>
+              <p>${products.keep_day} хоног</p>
+            </article>
+            <article>
+              <img src="/images/condition/celsius.png"></img>
+              <h3>ХАДГАЛАХ НӨХЦӨЛ</h3>
+              <p>${products.keep_cond}</p>
+            </article>
+          </section>
+          <section class="product-description">
+            <h2>БҮТЭЭГДЭХҮҮНИЙ ОНЦЛОГ:</h2>
+            <p>${products.prod_adventage}</p>
+          </section>
+          <section class="product-upside">
+            <h2>ДАВУУ ТАЛУУД:</h2>
+            <p>${products.pros}</p>
+          </section>
+        </section>
+      `;
+    }
+    
+  }
+  
+  // Define the custom element
+  customElements.define('product-details', ProductDetails);
+
+
+  
